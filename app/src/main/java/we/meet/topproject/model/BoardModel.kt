fa372275d4f62c4c7b239c6699db507711e1e74d
eogud7126 <eogud7126@gmail.com>
2020-05-26 17:59:03 +0900

    first commit

---
+++ app/src/main/java/we/meet/topproject/model/BoardModel.kt
@@ -0,0 +1,11 @@
+package we.meet.topproject.model
+
+data class BoardModel (
+    var title: String,
+    var description: String,
+    var createdDate: Long,
+    var commentCnt: Int,
+    var image: String)
+{
+    
+}
